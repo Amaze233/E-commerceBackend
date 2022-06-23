804a845a87610c5dd0f77611d6edc5a2ed1d40c9
肖思宇 <2019302110199@whu.edu.cn>
2022-06-23 17:11:17 +0800

    Initial commit

---
+++ src/main/java/com/docking/demo/controller/LoginController.java
@@ -0,0 +1,12 @@
+package com.docking.demo.controller;
+
+/**
+ * @ClassName: LoginController
+ * @Author: XIAO SIYU
+ * @Date: 2022/6/23 17:05
+ * @Version: 1.0
+ * @Description: TODO
+ */
+
+public class LoginController {
+}
